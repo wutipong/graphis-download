1fcf30843a8ca3bb84b5390d85f63a2562654cd0
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2019-03-21 00:08:58 +0700

    Activate the extension only in certain URLs.

--- background.js
+++ background.js
@@ -2,7 +2,19 @@
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'graphis.ne.jp' },
+            pageUrl: {
+                hostEquals: 'graphis.ne.jp',
+                pathEquals: '/monthly/model.php'
+            },
+        })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }, {
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+                hostEquals: 'graphis.ne.jp',
+                pathEquals: '/monthly/item.php'
+            },
         })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
