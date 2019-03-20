9b3b8b1563db614c13a189d2840e4ea1d032c284
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2019-03-21 04:13:03 +0700

    download on the model page completed.

--- model.js
+++ model.js
@@ -1,7 +1,31 @@
-(function(results) {
-    results.modelImgUrl = $("#model-box2").find("img").attr('src');
-    results.test = "Test";
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        //let $photoThumb = $(".photo-thumb")
+        var result = {
+            test: "Test",
+            urls: []
+        }
 
-    let $photoThumb = $(".photo-thumb")
-     
-})
+        result.urls.push({
+            name: "model.jpg",
+            url: $(".model-box2").find("img").attr('src'),
+        })
+
+        addUrlsFromGalleryBox(".photo-gallery-box", "pic", result.urls)
+        addUrlsFromGalleryBox(".video-gallery-box", "movs", result.urls)
+
+        sendResponse(result);
+    });
+
+function addUrlsFromGalleryBox(selector, name, array) {
+    var $photoThumb = $(selector).find('.photo-thumb');
+    var count = 1;
+    $photoThumb.children().each(function (i, div) {
+        let $img = $(div).find("ul > li > a > img");
+        array.push({
+            name: name + count + ".jpg",
+            url: $img.attr('src'),
+        });
+        count++;
+    });
+}
--- popup.js
+++ popup.js
@@ -8,33 +8,29 @@ $('#name').on('change', function (event) {
   name = name.replace('/', ' ');
   $('#name').val(name);
 
-  chrome.storage.local.set({ name: name }, function () {
+  chrome.storage.local.set({
+    name: name
+  }, function () {
     console.log(name);
   })
 });
 
-var urlStr;
-chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-  urlStr = tabs[0].url;
-})
-
 $("#download").on("click", function () {
-  var url = new URL(urlStr);
-  console.log(url.pathname);
-
-  switch (url.pathname) {
-    case "/monthly/model.php":
-      console.log("Downloading Model Page");
-      chrome.tabs.executeScript(null, {
-        file: 'model.js'
-      }, function (results) {
-        console.log(results.modelImgUrl);
-        console.log(results.test);
-      });
-      break;
-
-    case "/monthly/item.php":
-      console.log("Downloading Item Page");
-      break;
-  }
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      greeting: "hello"
+    }, function (response) {
+      console.log(response);
+      let name = $("#name").val();
+      response.urls.forEach(function (download) {
+        chrome.downloads.download({
+          url: download.url,
+          filename: name + "\\" + download.name
+        });
+      })
+    });
+  });
 })
