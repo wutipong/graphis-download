eef604ba07324f0a5b310c7fe5c5480c8f23449e
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2019-04-18 04:44:07 +0700

    Add history list

--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
-chrome.storage.local.get(['name'], function (data) {
+var historyValues = []
+
+chrome.storage.local.get(['name', 'history'], function (data) {
   $('#name').val(data.name)
-  console.log(data.name)
+  historyValues = data.history
+  if (historyValues == null) historyValues = []
+  updateHistory()
 })
+
+function updateHistory () {
+  let $historyList = $('#history-list')
+  $historyList.empty()
+  historyValues.forEach(function (history) {
+    $historyList.append('<option>' + history + '</option>')
+  })
+}
 
 $('#name').on('change', function (event) {
   let name = $('#name').val().replace('\\', ' ')
@@ -11,13 +23,17 @@ $('#name').on('change', function (event) {
 
   chrome.storage.local.set({
     name: name
-  }, function () {
-    console.log(name)
   })
 })
 
 $('#clear').on('click', function () {
   $('#name').val('')
+  $('#name').trigger('change')
+})
+
+$('#history-list').change(function () {
+  let $selectItem = $('#history-list option:selected')
+  $('#name').val($selectItem.text())
   $('#name').trigger('change')
 })
 
@@ -29,8 +45,23 @@ $('#download').on('click', function () {
     chrome.tabs.sendMessage(tabs[0].id, {
       greeting: 'hello'
     }, function (response) {
-      console.log(response)
       let name = $('#name').val()
+
+      if (historyValues.length === 0 ||
+        (historyValues.length > 0 && historyValues[0] !== name)) {
+        historyValues.unshift(name)
+
+        if (historyValues.length > 10) {
+          historyValues.length = 10
+        }
+
+        chrome.storage.local.set({
+          history: historyValues
+        })
+
+        updateHistory()
+      }
+
       response.forEach(function (download) {
         chrome.downloads.download({
           url: download.url,
